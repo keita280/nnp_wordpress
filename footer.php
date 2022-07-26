f55f73156fa04de5d09f64e1f1c31eeb033c812c
keita280 <tanakei1115@gmail.com>
2022-07-27 05:45:54 +0900

    headerとhooterを分割

---
+++ footer.php
@@ -0,0 +1,79 @@
+<!-- //＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠
+//footer
+//＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠ -->
+<section class="footer">
+  <div class="footer_contents bgc-green">
+    <div class="footer_contents_container inner">
+      <div class="footer_contents_adress">
+        <figure class="footer_logo"><img src="<?php echo get_template_directory_uri(); ?>/img/nnp_logo.png" alt=""></figure>
+        <div class="footer_contents_adress_texts">
+          <p>〒173-0037<br>
+            東京都板橋区小茂根4-11-11<br>
+            一般財団法人 精神医学研究所附属東京武蔵野病院 栄養科<br>
+            FAX：050-3730-7423<br>
+            TEL：03-5986-3171</p>
+        </div>
+        <!-- footer_contents_adress_texts -->
+      </div>
+      <!-- footer_contents_adress -->
+      <div class="footer_contents_links">
+        <ul class="footer_contents_link_top">
+          <li class="footer_contents_link_text"><a href="/about.html" >私たちについて</a></li>
+          <li class="footer_contents_link_text"><a href="/news.html" >新着情報</a></li>
+          <li class="footer_contents_link_text"><a href="/strong.html">私たちの強み</a></li>
+          <li class="footer_contents_link_text"><a href="/reference.html" >資料室</a></li>
+        </ul>
+        <!-- footer_contents_link_top -->
+        <ul class="footer_contents_link_bottom">
+          <li class="footer_contents_link_text"><a href="/message.html">ご挨拶</a></li>
+          <li class="footer_contents_link_text"><a href="/schedule.html" >研修会・イベント一覧</a></li>
+          <li class="footer_contents_link_text"><a href="/admission.html">入会案内</a></li>
+          <li class="footer_contents_link_text"><a href="/change.html" >会員情報の変更・退会</a></li>
+        </ul>
+        <!-- footer_contents_link_bottom -->
+      </div>
+      <!-- footer_contents_links -->
+    </div>
+    <!-- footer_container -->
+  </div>
+  <!-- footer_contents -->
+  <div class="footer_bottom bgc-deep-green">
+    <div class="footer_container inner">
+      <ul class="footer_bottom_left">
+        <li><a href="/contact.html" class="footer_bottom_left_link footer_bottom_left_link_left">お問い合わせ</a></li>
+        <li><a href="/privacy.html" class="footer_bottom_left_link footer_bottom_left_link_right">プライバシーポリシー</a></li>
+      </ul>
+      <div class="footer_bottom_right">Copyright © 2022<br class="is-sp"> 全国精神科栄養士協会 All Rights Reserved.</div>
+    </div>
+    <!-- footer_container -->
+  </div>
+    <!-- footer_bottom -->
+</section>
+
+
+<!-- //＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠
+//スクロールトップ
+//＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠ -->
+<!-- <figure class="scroll-top">
+  <a href="#"><img src="./img/icon_arrow-top.png" alt="#"></a>
+</figure> -->
+
+
+</main>
+
+
+<!-- swipeｒ -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.8.4/swiper-bundle.min.js"></script>
+
+
+<!-- jquery & iScroll -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/iScroll/5.1.3/iscroll.min.js"></script>
+<!-- drawer.js -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.1/js/drawer.min.js"></script>
+
+<script src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
+<?php wp_footer(); ?>
+</body>
+
+</html> 
---
+++ header.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" 
+    content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="一般社団法人　全国精神科栄養士協会 のホームページです。">
+    <meta name="robots" content="noindex">
+    <!-- 本番納品前にnoindex消すの忘れないこと！！！ -->
+    <title>一般社団法人　全国精神科栄養士協会 ｜ Nutrition Needs in Psychiatry</title>
+
+    <!-- reset css -->
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/reset.css">
+    <!-- favicon -->
+    <link rel="icon" href="#">
+    <!-- fontowsome -->
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    <!-- googlefont -->
+    <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Amatic+SC&family=Damion&family=Noto+Serif+JP&family=Patua+One&display=swap" rel="stylesheet"> -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Varela&display=swap" rel="stylesheet">
+
+
+
+
+    <!-- jquery & iScroll -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/iScroll/5.2.0/iscroll.min.js"></script>
+
+    <!-- original -->
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
+    <?php wp_head(); ?>
+  </head>
+  
+  
+<body class="">
+  <header class="accordion" id="header"> 
+    <div class="header_inner">
+      <div class="header_top">
+          <h1 class="header_top_left is-pc opacity"><a href="#">一般社団法人　全国精神科栄養士協会 ｜ Nutrition Needs in Psychiatry</a></h1>
+          <h1 class="header_top_left is-sp"><a href="#">一般社団法人　全国精神科栄養士協会<br>Nutrition Needs in Psychiatry</a></h1>
+          <nav class="header_top_right is-pc">
+            <ul class="header_top_right_menu">
+              <li class="header_top_right_list"><a href="/message.html" class="header_top_right_link  underline">ご挨拶</a></li>
+              <li class="header_top_right_list"><a href="/change.html" class="header_top_right_link">会員情報の変更・退会</a></li>
+              <li class="header_top_right_list"><a href="/contact.html" class="header_top_right_link">お問い合わせ</a></li>
+            </ul>
+          </nav>
+      </div>
+      <nav class="header_bottom is-pc">
+        <ul class="header_bottom_menu">
+          <li class="header_bottom_list"><a class="header_bottom_link" href="/about.html">私たちについて</a></li>
+          <li class="header_bottom_list"><a class="header_bottom_link" href="/news.html">新着情報</a></li>
+          <li class="header_bottom_list"><a class="header_bottom_link" href="/schedule.html">研修会・イベント一覧</a></li>
+          <li class="header_bottom_list"><a class="header_bottom_link" href="/reference.html">資料室</a></li>
+          <li class="header_bottom_list"><a class="header_bottom_link" href="/strong.html">私たちの強み</a></li>
+          <li class="join"><a class="header_bottom_link btn-green" href="/admission.html">入会案内</a></li>
+        </ul>
+      </nav>
+
+    </div>
+    <!-- @@@@@@@@@@@@@@@@@@@@@@@@@ -->
+    <!-- ハンバーガー -->
+  <!-- @@@@@@@@@@@@@@@@@@@@@@@@@ -->
+  <div class="hamburger is-sp  js-accordion-title" id="hamburger">
+    <span class="is-sp"></span>
+    <span></span>
+    <span></span>
+  </div>
+  
+  <nav class="accordion-item is-sp" id="accordion-item">
+    <ul class="accordion-item_menu">
+      <li class="accordion-item_list"><a class="accordion-item_link" href="/about.html">私たちについて<span class="arrow-right"></span></a></li>
+      <li class="accordion-item_list"><a class="accordion-item_link" href="/news.html">新着情報<span class="arrow-right"></span></a></li>
+      <li class="accordion-item_list"><a class="accordion-item_link" href="/schedule.html">研修会・イベント一覧<span class="arrow-right"></span></a></li>
+      <li class="accordion-item_list"><a class="accordion-item_link" href="/reference.html">資料室<span class="arrow-right"></span></a></li>
+
+      <li class="accordion-item_list"><a class="accordion-item_link" href="/strong.html">私たちの強み<span class="arrow-right"></span></a></li>
+      <li class="accordion-item_list"><a class="accordion-item_link" href="/admission.html">入会案内<span class="arrow-right"></span></a></li>
+    </ul>
+    <ul class="accordion-item_sub-menu">
+      <li class="accordion-item_sub-list"><a href="/message.html" class="accordion-item_sub-link">ご挨拶</a></li>
+      <li class="accordion-item_sub-list"><a href="/change.html" class="accordion-item_sub-link">会員情報の変更・退会</a></li>
+      <li class="accordion-item_sub-list"><a href="/contact.html" class="accordion-item_sub-link">お問い合わせ</a></li>
+    </ul>
+  </nav>
+</header>
