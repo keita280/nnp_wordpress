c4675865ca86f29f6ddec394609e22e23c217b86
keita280 <tanakei1115@gmail.com>
2022-07-29 05:17:55 +0900

    読み込み解決

--- functions.php
+++ functions.php
@@ -24,13 +24,13 @@ add_action('after_setup_theme', 'my_setup');
 */
 function my_script_init() {
   // reset_css
-  wp_enqueue_style('my', get_template_directory_uri() . '/css/reset.css', array(), filemtime(get_theme_file_path('css/reset.css')), 'all');
+  wp_enqueue_style('reset', get_template_directory_uri() . '/css/reset.css', array(), filemtime(get_theme_file_path('css/reset.css')), 'all');
   // fontawsome
   wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css', array(), '5.8.2', 'all');
   // original_css
   wp_enqueue_style('my', get_template_directory_uri() . '/css/style.css', array(), filemtime(get_theme_file_path('css/style.css')), 'all');
   // js
-  wp_enqueue_script('my', get_template_directory_uri() . '/js/script.js', array('jquery'), filemtime(get_theme_file_path('js/script.js')), true);
+  wp_enqueue_script('script', get_template_directory_uri() . '/js/script.js', array('jquery'), filemtime(get_theme_file_path('js/script.js')), true);
 }
 add_action('wp_enqueue_scripts', 'my_script_init');
 
