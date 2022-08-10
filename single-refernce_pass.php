f7655635a415748806286e1b3ff25b65f6692269
keita280 <tanakei1115@gmail.com>
2022-08-10 12:49:12 +0900

    資料室カスタム投稿完成

---
+++ single-refernce_pass.php
@@ -0,0 +1,103 @@
+<?php
+/*
+Template Name: reference-passページ
+*/
+get_header();
+?>
+
+
+
+
+
+
+
+
+
+    <!-- ページタイトル -->
+    <div class="page-title">
+      <figure class="section_title_blob-r">
+        <img src="<?php echo get_template_directory_uri() ;?>/img/blobs.svg" alt="" />
+      </figure>
+      <h1 class="page-ttl">資料室</h1>
+      <figure class="section_title_blob-l">
+        <img src="<?php echo get_template_directory_uri() ;?>/img/blobs_2.svg" alt="" />
+      </figure>
+    </div>
+
+
+    <main>
+      <!-- パンくずリスト -->
+      <div class="breadcrumb inner">
+        <ol class="breadcrumbs">
+          <li><a href="../index.html">トップ</a></li>
+          <li><a href="./index.html"></a>資料室</li>
+        </ol>
+      </div>
+
+      <!-- 資料室 -->
+      <section class="pass_container">
+        <div class="pass-content-description">
+          <p>
+            資料はダウンロードして、栄養指導や施設内業務にご活用いただけます。
+          </p>
+          <p>
+            学会発表や論文に載せるなど、資料を外部に公開する場合には、<a href=""
+              >お問い合わせフォーム</a
+            >より事務局にご連絡ください。<br /><br />
+          </p>
+          <p>このコンテンツは会員限定です。</p>
+          <p>非会員へダウンロードした資料を配布することはご遠慮ください。</p>
+        </div>
+        <ul class="document-list">
+        <?php $reference_group = SCF::get('reference-items'); ?>
+          <?php foreach ($reference_group as $fields ):?>
+
+          <li class="document-item">
+            <label class="document-title"><?php echo esc_html( $fields['reference_title']); ?></label
+            ><a class="document-title-link"
+            href="<?php echo esc_html( $fields['pdf_url']); ?>"
+            target="_blank"
+            style="text-decoration: underline"
+            ><?php echo esc_html( $fields['pdf_title']); ?></a>
+          </li>
+          <?php endforeach ?>
+
+          <!-- <li class="document-item">
+            <label class="document-title">○○媒体資料</label
+            ><a class="document-title-link"
+              href="nnp.pdf"
+              target="_blank"
+              style="text-decoration: underline"
+              >PDFタイトルを入れます</a
+            >
+          </li>
+          <li class="document-item">
+            <label class="document-title">○○媒体資料</label
+            ><a class="document-title-link"
+              href="nnp.pdf"
+              target="_blank"
+              style="text-decoration: underline"
+              >PDFタイトルを入れます</a
+            >
+          </li>
+          <li class="document-item">
+            <label class="document-title">○○媒体資料</label
+            ><a class="document-title-link"
+              href="nnp.pdf"
+              target="_blank"
+              style="text-decoration: underline"
+              >PDFタイトルを入れます</a
+            >
+          </li>
+          <li class="document-item">
+            <label class="document-title">○○媒体資料</label
+            ><a class="document-title-link"
+              href="nnp.pdf"
+              target="_blank"
+              style="text-decoration: underline"
+              >PDFタイトルを入れます</a
+            >
+          </li> -->
+        </ul>
+      </section>
+      <?php get_footer(); ?>
